ee8c84b3e558842bc10962e2f61060ae3c96131b
joao_v_guimaraes <vivito2@gmail.com>
2023-05-18 17:29:04 -0300

    4 versao

--- src/Player.java
+++ src/Player.java
@@ -141,10 +141,18 @@ public class Player {
             linhaDoTabuleiro = new StringBuilder((letraDaLinha++) + " |");
             for (int coluna : linha) {
                 switch (coluna) {
-                    case BatalhaNaval.VAZIO -> linhaDoTabuleiro.append(" |");
-                    case BatalhaNaval.NAVIO -> linhaDoTabuleiro.append("N|");
-                    case BatalhaNaval.ERROU_TIRO -> linhaDoTabuleiro.append("X|");
-                    case BatalhaNaval.ACERTOU_TIRO -> linhaDoTabuleiro.append("D|");
+                    case BatalhaNaval.VAZIO: {
+                        linhaDoTabuleiro.append(" |");
+                    }
+                    case BatalhaNaval.NAVIO : {
+                        linhaDoTabuleiro.append("N|");
+                    }
+                    case BatalhaNaval.ERROU_TIRO: {
+                        linhaDoTabuleiro.append("X|");
+                    }
+                    case BatalhaNaval.ACERTOU_TIRO : {
+                        linhaDoTabuleiro.append("D|");
+                    }
                 }
             }
             System.out.println(linhaDoTabuleiro);
